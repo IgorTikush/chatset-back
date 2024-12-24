d7ef6a740bf1cc42f175dd8f43318a5c1210dacd
igortikushEasync <igor@easync.io>
2024-12-24 22:36:05 +0100

    config

--- config/production.js
+++ config/production.js
@@ -6,4 +6,9 @@ config.jwtAccessSecret = process.env.JWT_ACCESS_SECRET;
 config.stabilityKey = process.env.STABILITY_KEY;
 config.geminiKey = process.env.GEMINI_KEY;
 
+config.cloudpayments = {
+  username: process.env.CLOUDPAYMENTS_USERNAME,
+  password: process.env.CLOUDPAYMENTS_PASSWORD,
+};
+
 module.exports = config;
